edcc83353c020fe25372f10180541e3fc04af5a9
Rohit <flux.amv25@gmail.com>
2026-03-29 09:03:08 +0530

    Updated main.ts : Added global exception handler (basic) and validation pipeline

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { AllExceptionFilter } from './config/allExceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist : true
+  }));
+  app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalPipes(new ValidationPipe()) // class validation
   await app.listen(process.env.PORT ?? 3000);
 }
