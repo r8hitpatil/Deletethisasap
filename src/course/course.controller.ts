bc051fab6790d094c747002b37f76fc822ca81fd
Rohit <flux.amv25@gmail.com>
2026-03-29 09:12:01 +0530

    Moved few files and deleted few files to clean up folder structure

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -2,20 +2,19 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } fro
 import { CourseService } from './course.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
-import { AuthGuard } from 'src/auth/auth.guard';
-import { Roles } from 'src/auth/role.decorator';
-import { Role } from 'src/auth/role.enum';
-import { RolesGuard } from 'src/auth/role.guard';
+import { JwtAuthGuard, RoleGuard } from 'src/auth/guards';
+import { Roles } from 'src/auth/decorators';
+import { Role } from '@prisma/client';
 
 @Controller('courses')
 export class CourseController {
   constructor(private readonly courseService: CourseService) {}
 
-  @UseGuards(AuthGuard, RolesGuard)
+  @UseGuards(JwtAuthGuard, RoleGuard)
   @Roles(Role.ADMIN)
   @Post('create')
   create(@Body() createCourseDto: CreateCourseDto, @Req() req) {
-    const userId = req.user.sub;
+    const userId = req.user.userId;
     return this.courseService.create(createCourseDto,userId);
   }
 
