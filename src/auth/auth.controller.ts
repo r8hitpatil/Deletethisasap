11b8c3347d097f7f317546cc70d5ce6a458f850e
Rohit <flux.amv25@gmail.com>
2026-04-13 06:18:17 +0530

    fix: no more explicit tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,27 @@
-import { Body, Controller, Get, HttpCode, Post, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from 'src/auth/dto/registerUser.dto';
 import { LoginDto } from './dto/loginUser.dto';
-import { JwtAuthGuard } from './guards';
 import type { Response } from 'express';
 import { GetUser } from './decorators';
-import type { JwtUser } from './types';
-import { setRefreshToken } from './utils/set-refresh-token';
-import { RefreshToken } from 'src/common/decorators';
+import type { JwtPayloadWithRt, JwtUser } from './types';
+import { setAccessToken, setRefreshToken } from './utils/cookie.utils';
+import { AtGuard, RtGuard } from './guards';
 
 @Controller('auth')
 export class AuthController {
 
     constructor(private readonly authService:AuthService){}
-    @HttpCode(201)
+    @HttpCode(HttpStatus.CREATED)
     @Post('register')
     async register(@Body() registerUserDto:RegisterDto, @Res({ passthrough : true }) res:Response){
         const tokens = await this.authService.registerUser(registerUserDto);
         
-        setRefreshToken(res,tokens.refreshToken);
+        setRefreshToken(res,tokens.rToken);
+        setAccessToken(res,tokens.aToken);
 
         return {
-            accessToken : tokens.accessToken
+            message : "User Registered successfully"
         }
     }
 
@@ -29,33 +29,33 @@ export class AuthController {
     async login(@Body() LoginDto:LoginDto,@Res({ passthrough : true }) res:Response){
         const tokens = await this.authService.loginUser(LoginDto);
         
-        setRefreshToken(res,tokens.refreshToken);
+        setRefreshToken(res,tokens.rToken);
+        setAccessToken(res,tokens.aToken);
 
         return {
-            accessToken : tokens.accessToken
+            message : "User logged in successfully"
         }
     }
 
+    @UseGuards(RtGuard)
     @Post('refresh')
     async newAccessToken(
-        @RefreshToken() refreshToken: string,
+        @GetUser() user: JwtPayloadWithRt,
         @Res({ passthrough : true }) res: Response,
     ){
-        const tokens = await this.authService.reAccessToken(refreshToken);
-
-        setRefreshToken(res,tokens.refreshToken);
+        const tokens = await this.authService.reAccessToken(user.refreshToken);
+        setRefreshToken(res,tokens.rToken);
+        setAccessToken(res,tokens.aToken);
 
         return {
-            accessToken : tokens.accessToken
+            message : "Tokens refreshed successfully"
         }
     }
 
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(AtGuard)
     @Get('profile')
     // why we imported JwtUser as type is we need it as a type during runtime before it was crashing like interface JwtUser {}
     async getProfile(@GetUser() user:JwtUser) {
-        return {
-            data : user
-        };
+        return user;
     }
 }
